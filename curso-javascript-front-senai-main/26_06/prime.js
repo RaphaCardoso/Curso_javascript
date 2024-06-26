046f823d6b5e9fc29d4437870f1d1a99b5d6c7f4
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-06-26 08:40:06 -0300

    adicionando var

--- curso-javascript-front-senai-main/26_06/prime.js
+++ curso-javascript-front-senai-main/26_06/prime.js
@@ -1,2 +1,8 @@
-console.log("teste");
-console.log("Rapha");
+// coments
+
+// let tipo de variável que após ser utilizada pode ter seu valor mudado
+// const tipo de variável nã alterável
+
+// alert("Olá, você é um erro!") função da linguagem para exibir uma caixa de texto
+// console.log('para imprimir algo no console')
+
---
+++ curso-javascript-front-senai-main/26_06/variavel.js
@@ -0,0 +1,9 @@
+// let mensagens = "hello, world"
+
+// let nome; // cria uma varável indefinido
+
+// let disciplina = null;
+
+// let texto = "Este é um exemplo"
+
+console.log(texto);
