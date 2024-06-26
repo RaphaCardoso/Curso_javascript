c60c3d73dc9603b1cb98bc62ce11747decd790e8
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-06-26 09:01:36 -0300

    adicionando more var

---
+++ curso-javascript-front-senai-main/26_06/tipo-dados.js
@@ -0,0 +1,24 @@
+// INTEIRO  
+
+let idadeAluno = 18;
+
+// Números float com casas decimais
+
+let valorProduto = 5.17;
+
+console.log("valor do produto R$ " + valorProduto + " esse é o valor");
+
+console.log(`valor do produto R$ ${valorProduto} esse é o valor`);
+
+console.log("R$ %s esse é o valor do produto, idade %s", valorProduto, idadeAluno);
+
+// variável de texto = string
+let nomeAluno = "Thiagão";
+
+console.log("nome do aluno %s", nomeAluno);
+
+// valor booleano true or false
+
+const casado = true;
+
+console.log("é casado?", casado);
