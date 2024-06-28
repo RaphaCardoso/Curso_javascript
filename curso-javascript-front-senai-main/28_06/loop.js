0bcc775fb619c37a1e0f2e73ec97f16294700511
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-06-28 11:07:52 -0300

    for add

--- curso-javascript-front-senai-main/28_06/loop.js
+++ curso-javascript-front-senai-main/28_06/loop.js
@@ -19,3 +19,10 @@ console.log(valor);
 valor *= 2;
 
 console.log(valor);
+
+
+// Aprendendo sobre o for
+//  inicialização ;      condição       ;    incremento
+for (let index = 0; index < array.length; index++) {
+    const eleent = arra[index]
+}
