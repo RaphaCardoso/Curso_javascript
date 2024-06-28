86dabd947562d06d0adaa0a2ef947ccb38b10eee
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-06-28 10:26:32 -0300

    form

--- curso-javascript-front-senai-main/27_06/sistema-aluno/script.js
+++ curso-javascript-front-senai-main/27_06/sistema-aluno/script.js
@@ -1,14 +1,50 @@
 const button = document.querySelector("button");
 const inputNota = document.querySelector("#nota")
 
-//Criando uma função
-button.addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log(inputNota.value);
+// criando função limpar campo
 
-    if (inputNota.value >= 6 ){
-        alert("Você está Aprovado");
-    } else(inputNota.value <= 5); {
-        alert("Você está Reprovado")
-    
-    }});
+function clearForm() {
+    // mudando o valor da caixa de texto
+    inputNota.value = "";
+}
+
+
+//Criando uma função
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const inputNotaValue = inputNota.value
+    console.log(inputNotaValue);
+
+    // validação de formulário
+
+    if (inputNotaValue === "") {
+        alert("Campo Obrigatório")
+        return false;
+    }
+
+    const tipo = parseInt(inputNotaValue) //typeof inputNotaValue;
+
+    const validar = isNaN(tipo)
+    console.log(validar);
+
+    if (validar) {
+        alert("Insira um número!");
+        return false;
+    }
+
+    if (inputNotaValue <= 11 && inputNotaValue >= 0) {
+        if (inputNotaValue >= 6) {
+            alert("Você está Aprovado")
+        } else if (inputNotaValue <= 5) {
+            alert("Você está Reprovado")
+        }
+    } else {
+        alert("nota inválida")
+    }
+
+
+    // chamando função limpar campo
+    clearForm()
+
+});
