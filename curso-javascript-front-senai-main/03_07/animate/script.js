7e23247d6a77b26b95f2ca6a5b2fbb2559fd287d
Raphael de Barros Cardoso Jr <154370014+RaphaCardoso@users.noreply.github.com>
2024-07-03 11:28:50 -0300

    mover rotate

--- curso-javascript-front-senai-main/03_07/animate/script.js
+++ curso-javascript-front-senai-main/03_07/animate/script.js
@@ -1,2 +1,25 @@
 const moverImagem = document.getElementById("btnMoverImagem");
-let imagem = document.getElementById("imagem")
+let imagem = document.getElementById("imagem");
+
+// var para rastrear o estado atual da posição da imagem
+let isMoved = false;
+
+moverImagem.addEventListener('click', function () {
+
+    if (!isMoved) {
+
+        isMoved = true;
+        imagem.style.transition = "1.2s"
+        imagem.style.transform = "rotate(50deg)"
+        imagem.style.left = "250px"
+        imagem.style.top = "250px"
+
+    } else if (isMoved) {
+        imagem.style.transition = "1.2s"
+        imagem.style.transform = ""
+        imagem.style.left = ""
+        imagem.style.top = ""
+        isMoved = false;
+    }
+
+})
